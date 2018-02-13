2577b04c48c1dc88a691f9c80def90fc385b5599
Spencer <spencerapel@gmail.com>
2018-02-12 20:27:58 -0600

    defined get_indetity, build_matrix, print_matrix and delete_matrix

--- maze.cpp
+++ maze.cpp
@@ -8,26 +8,39 @@ using std::string;
 
 void get_identity(string my_id[])
 {
+    my_id[0] = "sra7wb";
+    my_id[1] = "16231948";
+
+    //output
+    cout << "MST Campus Username: " << my_id[0] << endl;
+    cout << "Student ID: " << my_id[1] << endl;
 }
 
 
 string * build_matrix(int rows)
 {
+    string *matrix = new string *[rows];
+
+    return matrix;
 }
 
 
 void fill_matrix(string *matrix, int rows)
 {
+    //use getline
 }
 
 
 void print_matrix(string *matrix, int rows)
 {
+    for(int i = 0; i < rows; ++i)
+        cout << matrix[i] << endl;
 }
 
 
 void delete_matrix(string *matrix, int rows)
 {
+    delete[] matrix;
 }
 
 
